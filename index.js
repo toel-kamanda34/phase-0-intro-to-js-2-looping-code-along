24c4864d6f9185f263af349604c4b1fb0896b8ab
tkamanda <toelokemwa@gmail.com>
2024-07-21 18:13:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+//functin to write thank yo casrd 
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+
+}
+ writeCards(["Guadalupe","Ollie","Aki"]);
+//function to count down
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(10);
+countDown(4);
